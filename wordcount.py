aa8c36eb9bc120ddd5205401809ab60668aebcb3
Widzo1 <willsithole81@gmail.com>
2021-04-03 20:18:54 -0400

    adding wordcount.py

---
+++ wordcount.py
@@ -0,0 +1,22 @@
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+text = open("input.txt", "r")
+#empty dictionary 
+d = {}
+
+for line in text:
+  line = line.strip()
+  line = line.lower()
+  #splitting line into words
+  words = line.split(" ")
+  for word in words:
+    if word in d:
+      d[word] = d[word] + 1
+    else:
+      d[word] = 1
+      
+for key in list(d.keys()):
+  print(key, ":", d[key])
+      
