c1422f4cf91421d2446dc65c361da889797a2d91
Malyaka-Imran <55777834+Malyaka-Imran@users.noreply.github.com>
2021-04-18 19:13:14 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,307 @@
-# main.py 1.0.6
+#!/usr/bin/env python
+# coding: utf-8
 
+# In[1]:
+
+
+get_ipython().run_line_magic('pip', 'install frontend')
+get_ipython().run_line_magic('pip', 'install PyMuPDF')
+import fitz
 import numpy as np
 import sys
 import random
 import ea
-import fitz
+#import fitz
+get_ipython().run_line_magic('pip', 'install nltk')
 import nltk
+nltk.download('punkt')
+nltk.download('wordnet')
+get_ipython().run_line_magic('pip', 'install termcolor')
 from termcolor import colored
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
+
+
+from operator import itemgetter
+import json
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[2]:
+
+
+def fonts(doc, granularity=False):
+
+    """Extracts fonts and their usage in PDF documents.
+
+
+
+    :param doc: PDF document to iterate through
+
+    :type doc: <class 'fitz.fitz.Document'>
+
+    :param granularity: also use 'font', 'flags' and 'color' to discriminate text
+
+    :type granularity: bool
+
+
+
+    :rtype: [(font_size, count), (font_size, count}], dict
+
+    :return: most used fonts sorted by count, font style information
+
+    """
+
+    styles = {}
+
+    font_counts = {}
+
+
+
+    for page in doc:
+
+        blocks = page.getText("dict")["blocks"]
+
+        for b in blocks:  # iterate through the text blocks
+
+            if b['type'] == 0:  # block contains text
+
+                for l in b["lines"]:  # iterate through the text lines
+
+                    for s in l["spans"]:  # iterate through the text spans
+
+                        if granularity:
+
+                            identifier = "{0}_{1}_{2}_{3}".format(s['size'], s['flags'], s['font'], s['color'])
+
+                            styles[identifier] = {'size': s['size'], 'flags': s['flags'], 'font': s['font'],
+
+                                                  'color': s['color']}
+
+                        else:
+
+                            identifier = "{0}".format(s['size'])
+
+                            styles[identifier] = {'size': s['size'], 'font': s['font']}
+
+
+
+                        font_counts[identifier] = font_counts.get(identifier, 0) + 1  # count the fonts usage
+
+
+
+    font_counts = sorted(font_counts.items(), key=itemgetter(1), reverse=True)
+
+
+
+    if len(font_counts) < 1:
+
+        raise ValueError("Zero discriminating fonts found!")
+
+
+
+    return font_counts, styles
+
+
+# In[3]:
+
+
+def font_tags(font_counts, styles):
+
+    """Returns dictionary with font sizes as keys and tags as value.
+
+
+
+    :param font_counts: (font_size, count) for all fonts occuring in document
+
+    :type font_counts: list
+
+    :param styles: all styles found in the document
+
+    :type styles: dict
+
+
+
+    :rtype: dict
+
+    :return: all element tags based on font-sizes
+
+    """
+
+    p_style = styles[font_counts[0][0]]  # get style for most used font by count (paragraph)
+
+    p_size = p_style['size']  # get the paragraph's size
+
+
+
+    # sorting the font sizes high to low, so that we can append the right integer to each tag
+
+    font_sizes = []
+
+    for (font_size, count) in font_counts:
+
+        font_sizes.append(float(font_size))
+
+    font_sizes.sort(reverse=True)
+
+
+
+    # aggregating the tags for each font size
+
+    idx = 0
+
+    size_tag = {}
+
+    for size in font_sizes:
+
+        idx += 1
+
+        if size == p_size:
+
+            idx = 0
+
+            size_tag[size] = '<p>'
+
+        if size > p_size:
+
+            size_tag[size] = '<h{0}>'.format(idx)
+
+        elif size < p_size:
+
+            size_tag[size] = '<s{0}>'.format(idx)
+
+
+
+    return size_tag
+
+
+# In[4]:
+
+
+def headers_para(doc, size_tag):
+
+    """Scrapes headers & paragraphs from PDF and return texts with element tags.
+
+
+
+    :param doc: PDF document to iterate through
+
+    :type doc: <class 'fitz.fitz.Document'>
+
+    :param size_tag: textual element tags for each size
+
+    :type size_tag: dict
+
+
+
+    :rtype: list
+
+    :return: texts with pre-prended element tags
+
+    """
+
+    header_para = []  # list with headers and paragraphs
+
+    first = True  # boolean operator for first header
+
+    previous_s = {}  # previous span
+
+
+
+    for page in doc:
+
+        blocks = page.getText("dict")["blocks"]
+
+        for b in blocks:  # iterate through the text blocks
+
+            if b['type'] == 0:  # this block contains text
+
+
+
+                # REMEMBER: multiple fonts and sizes are possible IN one block
+
+
+
+                block_string = ""  # text found in block
+
+                for l in b["lines"]:  # iterate through the text lines
+
+                    for s in l["spans"]:  # iterate through the text spans
+
+                        if s['text'].strip():  # removing whitespaces:
+
+                            if first:
+
+                                previous_s = s
+
+                                first = False
+
+                                block_string = size_tag[s['size']] + s['text']
+
+                            else:
+
+                                if s['size'] == previous_s['size']:
+
+
+
+                                    if block_string and all((c == "|") for c in block_string):
+
+                                        # block_string only contains pipes
+
+                                        block_string = size_tag[s['size']] + s['text']
+
+                                    if block_string == "":
+
+                                        # new block has started, so append size tag
+
+                                        block_string = size_tag[s['size']] + s['text']
+
+                                    else:  # in the same block, so concatenate strings
+
+                                        block_string += " " + s['text']
+
+
+
+                                else:
+
+                                    header_para.append(block_string)
+
+                                    block_string = size_tag[s['size']] + s['text']
+
+
+
+                                previous_s = s
+
+
+
+                    # new block started, indicating with a pipe
+
+                    block_string += "|"
+
+
+
+                header_para.append(block_string)
+
+
+
+    return header_para
+
+
+# In[13]:
+
+
+# main.py 1.0.6
 
 ##list of logical words
 # comparison = {'similarily', 'likewise', 'also', 'comparison'}
@@ -17,8 +310,6 @@ from nltk.tokenize import word_tokenize
 # contrast = {'however', 'on the other hand', 'on the contrary', 'contrast'}
 # sequential = {'Firstly', 'secondly', 'thirdly', 'next','last', 'finally', 'in addition', 'furthermore', 'also','before', }
 # order_of_importance = {'most', 'more', 'importantly', 'significantly','above', 'all', 'primarily', 'essential'}
-
-
 
 
 doc = fitz.open("first_two_pages.pdf")
@@ -31,8 +322,25 @@ print(colored('hello', 'red'), colored('world', 'green'))
 print(content)
 print(colored('hello', 'red'), colored('world', 'green'))
 # sys.exit()
+
+font_counts, styles = fonts(doc, granularity=False)
+size_tag = font_tags(font_counts, styles)
+elements = headers_para(doc, size_tag)
+
+paragraphs= ""
+
+for elem in elements:
+    if(len(elem)>=4 and elem[1]=='p'):
+        paragraphs+=elem
+    
+
+print(colored('just the paragraphs', 'red'))
+print(paragraphs)
+print(colored("just the paragraphs", 'red'))
+
+
 ## removing stop words and Lemmatization
-stop_words=set(['a', 'about', 'above', 'after', 'again', 'against',
+stop_words=set([ 'a', 'about', 'above', 'after', 'again', 'against',
 	'all', 'am', 'an', 'and', 'any', 'are',
 	'as', 'at', 'be', 'because', 'been', 'before', 'being', 'below', 'between', 'both', 'but', 'by',
 	'could', 'did','do', 'does','doing','down', 'during', 'each', 'few', 'for', 'from', 'further', 'had','has','have','having', 'he', "he'd", "he'll", "he's", 'her', 'here', "here's", 'hers', 'herself', 'him', 'himself', 'his', 'how', "how's", 'i', "i'd", "i'll", "i'm", "i've", 'if', 'in', 'into', 'is',
@@ -46,32 +354,15 @@ lemmatizer = WordNetLemmatizer()
 filtered_sentence = []
 for w in word_tokens:
 	if w.lower() not in stop_words:
-		filtered_sentence.append(lemmatizer.lemmatize(w))
+		filtered_sentence.append(lemmatizer.lemmatize(w)
+
 final_sentence= ' '.join(filtered_sentence)
 initial_list_of_sentences = [sentence for sentence in final_sentence.split(".") if len(sentence) > 0]
+                                 
+                               
+#the following extra information is already removed when we remove headers and footers and extract the paragraphs above. 
+#so this chunk of code is probably not needed anymore. 
 
-
-
-extra_information= {"content downloaded", "http", "/", "Vol", "©"}
-
-
-list_of_sentences = []
-
-for sentence in range(len(initial_list_of_sentences)):
-	flag = True;
-	word_list = initial_list_of_sentences[sentence].split()
-	number_of_words = len(word_list)
-	for word in word_list:
-		if word in extra_information:
-			flag = False
-			continue
-	print(flag)
-	if number_of_words > 5 and flag == True:
-		list_of_sentences.append(initial_list_of_sentences[sentence])
-
-
-
-print(list_of_sentences)
 
 
 
@@ -190,3 +481,16 @@ for x in my_terms:
 
 
 doc.save("output.pdf", garbage=4, deflate=True, clean=True)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
